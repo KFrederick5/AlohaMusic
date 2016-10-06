6e9f8d6058b7a44fb10d8ac87edd2bb12348f1d0
kfrederick5 <tefsuncle@gmail.com>
2016-10-06 16:43:39 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/alohamusic/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package edu.orangecoastcollege.cs273.kfrederick5.alohamusic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Define timer task to launch media activity
+        TimerTask mediaTask = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashScreenActivity.this,
+                        MediaActivity.class));
+            }
+        };
+
+            Timer timer = new Timer();
+            timer.schedule(mediaTask, 5000);
+    }
+}
